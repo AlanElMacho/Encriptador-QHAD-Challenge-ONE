019f3869b876eda889ff0cd989dfc33ade336d11
AlanElMacho <38467231+AlanElMacho@users.noreply.github.com>
2022-11-07 12:03:06 -0600

    More security
    This is a much better form of encryption and decryption, which in turn is more secure and it would only be necessary to adapt the way of calling them

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function ocultarFrente(){
 }
 
 //encripta el mensaje
-function encriptarTexto(mensaje){
+/*function encriptarTexto(mensaje){
     var texto = mensaje;
     var textoFinal = "";
 
@@ -101,7 +101,79 @@ function desencriptarTexto(mensaje){
         }
     }
     return textoFinal;
-}
+}*/
+
+var Base64 = {
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    // public method for encoding
+    encode : function (mensaje) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+            Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
+            Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
+        }
+
+        return output;
+    },
+
+    // public method for decoding
+    decode : function (mensaje) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = Base64._keyStr.indexOf(input.charAt(i++));
+            enc2 = Base64._keyStr.indexOf(input.charAt(i++));
+            enc3 = Base64._keyStr.indexOf(input.charAt(i++));
+            enc4 = Base64._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+    }
 
 //COPIAR
 function copiar(){
